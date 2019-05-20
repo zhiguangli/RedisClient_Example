e4bc62ae1c2ff69c6a149d218798b8721ed50460
zhiguangli <252859182@qq.com>
2019-05-20 12:03:40 +0800

    Create example_usage.cpp

---
+++ example_usage.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "response_parser.h"
+
+using namespace std;
+
+
+
+int main()
+{
+
+    std::string content = "*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n";
+
+    auto replyItem = AbstractReplyType::createType(content.at(0));
+
+    if(!replyItem)
+    {
+        for(auto i = 1; i < content.size(); ++i)
+        {
+            switch(replyItem->feed(content.at(i)))
+            {
+                case ParseResult::Error:
+                    // reply error string to consloe.
+                    break;
+                case ParseResult::Finished:
+                    // reply to console.
+                    break;
+            }
+        }
+    }
+
+    return 0;
+}
